349b0204e35d2e0e410a999d159b6a17decd4b49
Beste Kocaoğlu <78082138+besteko@users.noreply.github.com>
2023-02-03 14:56:00 +0300

    Initial Commit

---
+++ BirthdayNoteTaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayNoteTaker
+//
+//  Created by Beste Kocaoglu on 3.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
